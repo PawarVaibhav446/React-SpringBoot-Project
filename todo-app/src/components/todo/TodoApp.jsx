7d7945931132a437ef5e2147313fa08d7cdff61a
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-02-09 11:52:07 +0530

    Completed Route Auth

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider, { useAuth } from './security/AuthContext'
 
 import LogoutComponent from "./LogoutComponent";
 import HeaderComponent from "./HeaderComponent";
@@ -12,6 +13,16 @@ import AuthProvider from "./security/AuthContext";
 import "./TodoApp.css";
 
 export default function TodoApp() {
+
+    function AuthenticatedRoute({ children }) {
+        const authContext = useAuth()
+
+        if (authContext.isAuthenticated)
+            return children
+
+        return <Navigate to="/" />
+    }
+
     return (
         <div className="TodoApp">
             <AuthProvider>
@@ -20,10 +31,27 @@ export default function TodoApp() {
                     <Routes>
                         <Route path="/" element={<LoginComponent />} />
                         <Route path="/login" element={<LoginComponent />} />
-                        <Route path="/welcome/:username" element={<WelcomeComponent />} />
+
+                        <Route path='/welcome/:username' element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>
+                        } />
+
                         <Route path="*" element={<ErrorComponent />} />
-                        <Route path='/logout' element={<LogoutComponent />} />
-                        <Route path="/todos" element={<ListTodosComponent />} />
+                        
+                        <Route path='/logout' element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>
+                        } />
+
+                        <Route path='/todos' element={
+                            <AuthenticatedRoute>
+                                <ListTodosComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        
                     </Routes>
                 </BrowserRouter>
             </AuthProvider>
